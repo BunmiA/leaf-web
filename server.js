9a575816d80a2320e779c1e37d4fe06f5d085d20
bunmiaworanti <olubunmi.aworanti@gmail>
2018-08-06 23:16:31 +0100

    fixing issue with slash attempt using * for get

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.use(bodyParser.json());
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
 
+app.use('*', function(req, res) {
+  res.sendfile('src/index.html')
+})
+
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
