d4f7eafc3c823d42a53e8bf40ae8c97934e06d7e
bunmiaworanti <olubunmi.aworanti@gmail>
2018-08-06 23:05:37 +0100

    fixing issue with slash

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(bodyParser.json());
+
+// Create link to Angular build directory
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
 
-// app.use('/*', function(req,res) {
-//   // res.sendfile(__dirname + '/src/index.html');
-//
-//   res.sendFile(__dirname+'/dist/src/index.html');
-// });
+
+
