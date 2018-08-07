7987a58ba020f76e3cf206fb7b9d6c0e9567baeb
bunmiaworanti <olubunmi.aworanti@gmail>
2018-08-08 00:18:55 +0100

    fixing active link issue with routerLinkActive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,9 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
-import {Observable} from 'rxjs';
-import {Location, LocationStrategy, PathLocationStrategy} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  // providers: [Location, {provide: LocationStrategy, useClass: PathLocationStrategy}],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  title: string;
-  //
-  // constructor(private location: Location) {
-  //   console.log(this.location.path());
-  //   this.title = this.location.path().substring(1);
-  // }
-
-
-  updateTitle(title) {
-    this.title = title;
-  }
-
-
-
 }
